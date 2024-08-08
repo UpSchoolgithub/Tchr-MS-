c5249dc1b6526c6aa3bef3febe8873dcc63026b0
Bhavya <datamanagement@up.school>
2024-08-08 11:02:02 +0530

    ProtectedRoute

--- manager-frontend/src/ProtectedRoute.js
+++ manager-frontend/src/ProtectedRoute.js
@@ -1,11 +1,14 @@
 // src/ProtectedRoute.js
 import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { useManagerAuth } from './context/ManagerAuthContext';
 
 const ProtectedRoute = ({ element }) => {
   const { token } = useManagerAuth();
-  return token ? element : <Navigate to="/dashboard" />;
+  const location = useLocation();
+
+  // If there's no token, redirect to login with the state of the current path
+  return token ? element : <Navigate to="/dashboard" state={{ from: location }} replace />;
 };
 
 export default ProtectedRoute;
