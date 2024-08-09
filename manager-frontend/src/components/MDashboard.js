beb2ee49a02aaff0ebedda9afb1dd936a5e34793
Bhavya <datamanagement@up.school>
2024-08-09 10:23:10 +0530

    ..

--- manager-frontend/src/components/MDashboard.js
+++ manager-frontend/src/components/MDashboard.js
@@ -31,17 +31,17 @@ const MDashboard = () => {
 
   return (
     <div className="dashboard-container">
-      <div className="gif-container">
-        <img src="/f841ac2befaedda240c55a06b23b33ec.gif" alt="Welcome" className="full-page-gif" />
-        
-      </div>
-      {!isLoading && (
+            {!isLoading && (
         <div className="welcome-message">
           <h2>Welcome, Manager!</h2>
           <p>We're glad to have you back. Here's your dashboard.</p>
           {/* You can display WebSocket data here if needed */}
         </div>
       )}
+      <div className="gif-container">
+        <img src="/f841ac2befaedda240c55a06b23b33ec.gif" alt="Welcome" className="full-page-gif" />
+        
+      </div>
     </div>
   );
 };
