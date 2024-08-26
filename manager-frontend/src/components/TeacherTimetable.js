e77e0f54b302784799b34c0922b5295c53b283dd
Bhavya <datamanagement@up.school>
2024-08-26 14:48:40 +0530

    ..

--- manager-frontend/src/components/TeacherTimetable.js
+++ manager-frontend/src/components/TeacherTimetable.js
@@ -1,3 +1,4 @@
+// src/components/TeacherTimetable.js
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axiosInstance from '../services/axiosInstance';
