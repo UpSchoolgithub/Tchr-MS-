972eaa1436d9fd7d1658ea8cf5db66a7c361035c
Bhavya <datamanagement@up.school>
2024-09-05 20:27:12 +0530

    .

--- Backend/app.js
+++ Backend/app.js
@@ -33,11 +33,12 @@ const assignmentRoutes = require('./routes/assignmentRoutes');
 const testRoutes = require('./routes/testRoutes');
 //const studentRoutes = require('./routes/student');
 const studentRoutes = require('./routes/students');
-//const sectionsRouter = require('./routes/students');
-//const studentsRouter = require('./routes/students')
+const sectionsRouter = require('./routes/students');
+const studentsRouter = require('./routes/students')
 const TimetablePeriods = require('./models/TimetablePeriods');
 
 app.use('/api/timetable', require('./routes/timetableRoutes'));
+
 // Teacher Routes
 const teacherAuthRoutes = require('./routes/teacherRoutes'); 
 const teacherRoutes = require('./routes/teacherRoutes');
@@ -552,7 +553,6 @@ app.use('/api', studentsRouter); // Ensure the route is prefixed correctly
 app.use('/api/teacher', teacherRoutes);
 app.use('/api/teachers', teachersRoutes);
 app.use('/api/timetable', timetableRoutes);
-//app.use('/timetable', timetableRoutes);
 
 // Fallback for undefined routes
 app.use((req, res) => {
