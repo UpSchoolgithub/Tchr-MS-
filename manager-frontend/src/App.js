90e70b196794ee0df8940c0baeec6ec33966dc08
Bhavya <datamanagement@up.school>
2024-09-19 16:09:52 +0530

    .

--- manager-frontend/src/App.js
+++ manager-frontend/src/App.js
@@ -63,28 +63,21 @@ function AppContent() {
       <div className="main-content">
         <Routes>
           <Route path="/" element={<Navigate to={token ? '/dashboard' : '/mlogin'} replace />} />
-
           <Route path="/mlogin" element={<MLoginForm />} />
 
           {/* Protected Routes */}
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <MDashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/classroom"
-            element={
-              <ProtectedRoute>
-                <MClassroom />
-              </ProtectedRoute>
-            }
-          />
-          {/* Other protected routes can be added similarly... */}
+          <Route path="/dashboard" element={<ProtectedRoute element={<MDashboard />} />} />
+          <Route path="/classroom" element={<ProtectedRoute element={<MClassroom />} />} />
+          <Route path="/view-teachers" element={<ProtectedRoute element={<MViewTeachers />} />} />
+          <Route path="/request" element={<ProtectedRoute element={<MRequest />} />} />
+          <Route path="/view-activities" element={<ProtectedRoute element={<MViewActivities />} />} />
+          <Route path="/dashboard/school/:schoolId/class/:classId/section/:sectionName" element={<ProtectedRoute element={<MSchoolClassSection />} />} />
+          <Route path="/teachers" element={<ProtectedRoute element={<TeacherList />} />} />
+          <Route path="/teachers/create" element={<ProtectedRoute element={<CreateTeacher />} />} />
+          <Route path="/teachers/edit/:id" element={<ProtectedRoute element={<EditTeacher />} />} />
+          <Route path="/dashboard/school/:schoolId/class/:classId/section/:sectionName/calendar" element={<ProtectedRoute element={<SchoolCalendar />} />} />
 
+          {/* 404 Page */}
           <Route path="*" element={<div>Page Not Found</div>} />
         </Routes>
       </div>
