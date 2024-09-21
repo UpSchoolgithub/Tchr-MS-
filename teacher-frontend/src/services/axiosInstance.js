b74ad886619a5bf293a6faeb93ebb7ece3980f15
Bhavya <datamanagement@up.school>
2024-09-21 12:05:30 +0530

    .

--- teacher-frontend/src/services/axiosInstance.js
+++ teacher-frontend/src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://tms.up.school/api', // Updated to match your backend URL
+  baseURL: 'https://tms.up.school/api',
 });
 
 axiosInstance.interceptors.request.use((config) => {
