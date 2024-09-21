7b99fb116f723461f3b96b037238300a067837af
Bhavya <datamanagement@up.school>
2024-09-21 22:23:54 +0530

    cors

--- Backend/middleware/cors.js
+++ Backend/middleware/cors.js
@@ -3,24 +3,24 @@ const cors = require('cors');
 
 // Define the list of allowed origins
 const allowedOrigins = [
-  'https://sm.up.school',
-  'https://teachermanager.up.school',
-  'https://myclasses.up.school',
-];
-
-// Define CORS options
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
+    'https://sm.up.school',
+    'https://teachermanager.up.school',
+    'https://myclasses.up.school',
+  ];
+  
+  const corsOptions = {
+    origin: function (origin, callback) {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    optionsSuccessStatus: 200,
+  };
+  
 
 module.exports = cors(corsOptions);
