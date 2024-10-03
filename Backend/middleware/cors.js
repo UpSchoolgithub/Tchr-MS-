856700e68d0acc9cc41311c449e7558f387999f2
Bhavya <datamanagement@up.school>
2024-10-03 20:30:18 +0530

    cors

--- Backend/middleware/cors.js
+++ Backend/middleware/cors.js
@@ -1,26 +1,23 @@
-// middleware/cors.js
 const cors = require('cors');
 
-// Define the list of allowed origins
 const allowedOrigins = [
     'https://sm.up.school',
     'https://teachermanager.up.school',
-    'https://myclasses.up.school'
-  ];
-  
-  // CORS configuration options
+    'https://myclasses.up.school',
+    'https://tms.up.school'  // Ensure this is included if it should be allowed
+];
+
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      console.log('Blocked origin:', origin); // Debug blocked origin
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
-  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'], // Allowed headers
-  credentials: true, // Allow sending cookies and credentials
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+    credentials: true
 };
 
 module.exports = cors(corsOptions);
