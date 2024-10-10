947906c732bc8a8fe10b42bd33fb66eb83ecbff9
Bhavya <datamanagement@up.school>
2024-10-10 18:43:46 +0530

    0

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 
 // Utility function to read users from the JSON file
 const getUsers = () => {
