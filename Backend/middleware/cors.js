d3006e30e21fdfff78b6646bc39f9f2948425dd3
Bhavya <datamanagement@up.school>
2024-11-07 10:56:23 +0530

    .

--- Backend/middleware/cors.js
+++ Backend/middleware/cors.js
@@ -15,10 +15,10 @@ const corsOptions = {
             callback(new Error('Not allowed by CORS'));
         }
     },
-    credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: ['Authorization', 'Content-Type', 'X-Requested-With', 'Accept'],
-    optionsSuccessStatus: 200
+    credentials: true, // Allows credentials to be sent from frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Defines allowed HTTP methods
+    allowedHeaders: ['Authorization', 'Content-Type', 'X-Requested-With', 'Accept'], // Defines allowed headers
+    optionsSuccessStatus: 200 // Ensures successful response for OPTIONS requests
 };
 
 module.exports = cors(corsOptions);
