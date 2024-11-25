72d4aa79b3105e11abe31749d275ea9badcf08e9
Bhavya <datamanagement@up.school>
2024-11-25 11:52:47 +0530

    d

--- teacher-frontend/src/components/TeacherSessions.js
+++ teacher-frontend/src/components/TeacherSessions.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useCallback, useRef } from 'react';
 import axiosInstance from '../services/axiosInstance';
 import { useParams, useNavigate } from 'react-router-dom';
 import DatePicker from 'react-datepicker';
