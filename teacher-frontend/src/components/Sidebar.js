91e0f05f1f4a8080d4b6e6cefba1e7e21a84165b
Bhavya <datamanagement@up.school>
2024-11-25 19:18:57 +0530

    d

--- teacher-frontend/src/components/Sidebar.js
+++ teacher-frontend/src/components/Sidebar.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaTachometerAlt, FaCalendarAlt, FaEnvelope, FaTasks, FaChalkboardTeacher } from 'react-icons/fa';
+import { FaTachometerAlt, FaCalendarAlt, FaEnvelope, FaTasks, FaChalkboardTeacher, FaSignOutAlt } from 'react-icons/fa';
 import { useTeacherAuth } from '../context/TeacherAuthContext';
 import './Sidebar.css';
 
 const Sidebar = () => {
-  const { teacherId } = useTeacherAuth(); // Assuming teacherId is available in the context
+  const { teacherId, logout } = useTeacherAuth(); // Access the logout function from the context
 
   return (
     <div className="sidebar">
@@ -13,43 +13,58 @@ const Sidebar = () => {
       <div className="sidebar-logo">
         <img src="/Upschool_2x.png" alt="UpSchool Logo" />
       </div>
+
       <ul>
+        {/* Dashboard */}
         <li>
-          <Link to="/dashboard">Dashboard
+          <Link to="/dashboard">
             <FaTachometerAlt className="icon" />
-            <li>
-        </li>
+            <span>Dashboard</span>
           </Link>
         </li>
-        {/* New Teacher Sessions Link */}
+
+        {/* Teacher Sessions */}
         {teacherId && (
           <li>
             <Link to={`/teacherportal/${teacherId}/teacher-sessions`}>
               <FaChalkboardTeacher className="icon" />
-              Teacher Sessions
+              <span>Teacher Sessions</span>
             </Link>
           </li>
         )}
+
+        {/* School Calendar */}
         <li>
           <Link to="/school-calendar">
             <FaCalendarAlt className="icon" />
-            School Calendar
+            <span>School Calendar</span>
           </Link>
         </li>
+
+        {/* Request */}
         <li>
           <Link to="/request">
             <FaEnvelope className="icon" />
-            Request
+            <span>Request</span>
           </Link>
         </li>
+
+        {/* View Activities */}
         <li>
           <Link to="/view-activities">
             <FaTasks className="icon" />
-            View Activities
+            <span>View Activities</span>
           </Link>
         </li>
-        
       </ul>
+
+      {/* Logout Button */}
+      <div className="sidebar-bottom">
+        <button className="logout-button" onClick={logout}>
+          <FaSignOutAlt className="icon" />
+          <span>Logout</span>
+        </button>
+      </div>
     </div>
   );
 };
