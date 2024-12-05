5a6a6f31a7fdacc20d819e2d7863bfbab9942987
Bhavya <datamanagement@up.school>
2024-12-05 14:16:02 +0530

    ds

--- frontend/src/components/SessionManagement.js
+++ frontend/src/components/SessionManagement.js
@@ -1,3 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams, Link } from 'react-router-dom'; // Add useParams
+
 const SessionManagement = () => {
   const { schoolId, classId, sectionId, subjectId } = useParams();
   const [sessions, setSessions] = useState([]);
