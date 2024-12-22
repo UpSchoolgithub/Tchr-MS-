382281abbfd127dacf20a4a414d0500be91d0184
Bhavya <datamanagement@up.school>
2024-12-22 22:08:34 +0530

    5tn

--- frontend/src/components/SessionPlan.js
+++ frontend/src/components/SessionPlan.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams, useLocation } from "react-router-dom";
-import { Modal as BootstrapModal, Button, Form } from "react-bootstrap";
+import { Modal, Button, Form } from "react-bootstrap";
 import "../styles.css";
 import { jsPDF } from "jspdf";
 
