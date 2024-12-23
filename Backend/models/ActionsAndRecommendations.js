fa5f282f393b61ef95caa0559e4415a47b7abd0b
Bhavya <datamanagement@up.school>
2024-12-23 12:13:13 +0530

    6fe

--- Backend/models/ActionsAndRecommendations.js
+++ Backend/models/ActionsAndRecommendations.js
@@ -5,8 +5,6 @@ class ActionsAndRecommendations extends Model {
   static associate(models) {
     this.belongsTo(models.SessionPlan, { foreignKey: 'sessionPlanId', as: 'SessionPlan' });
     this.belongsTo(models.Session, { foreignKey: 'sessionId', as: 'Session' });
-    this.belongsTo(models.Chapter, { foreignKey: 'chapterId', as: 'Chapter' });
-    this.belongsTo(models.Unit, { foreignKey: 'unitId', as: 'Unit' });
     this.hasOne(models.LessonPlansForActionsAndRecommendations, {
       foreignKey: 'actionsAndRecommendationsId',
       as: 'LessonPlan',
@@ -24,22 +22,18 @@ ActionsAndRecommendations.init(
     sessionPlanId: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      references: { model: 'SessionPlans', key: 'id' },
+      references: {
+        model: 'SessionPlans', // References SessionPlans table
+        key: 'id',
+      },
     },
     sessionId: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      references: { model: 'Sessions', key: 'id' },
-    },
-    chapterId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: { model: 'Chapters', key: 'id' },
-    },
-    unitId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: { model: 'Units', key: 'id' },
+      references: {
+        model: 'Sessions', // References Sessions table
+        key: 'id',
+      },
     },
     type: {
       type: DataTypes.ENUM('pre-learning', 'post-learning'),
