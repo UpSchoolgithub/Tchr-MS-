9b7b852be89a065231469576213d207643d72db3
Bhavya <datamanagement@up.school>
2025-01-04 13:11:48 +0530

    wdss85e

--- Backend/models/SessionPlan.js
+++ Backend/models/SessionPlan.js
@@ -1,3 +1,6 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/db'); // Ensure this points to your sequelize instance
+
 SessionPlan.init(
   {
     sessionId: {
@@ -39,3 +42,6 @@ SessionPlan.init(
     freezeTableName: true,
   }
 );
+
+
+module.exports = SessionPlan;
