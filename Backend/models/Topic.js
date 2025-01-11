9d22b2efcc246c8f8d0c9cfe44c468505cef47aa
Bhavya <datamanagement@up.school>
2025-01-11 17:59:27 +0530

    hncskm35445

--- Backend/models/Topic.js
+++ Backend/models/Topic.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
+const Concept = require('./Concept'); // Correct import
 
 class Topic extends Model {
   static associate(models) {
